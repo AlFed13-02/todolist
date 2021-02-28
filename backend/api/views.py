c76fde6fa5fa0a1f496b532220af800f0218367d
Aleksey Fedorov <alfed13.02@mail.ru>
2021-02-28 17:32:31 +0300

    Views for obtaining user related task list and task detail were implemented.

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.conf import settings
+from rest_framework import generics
+from rest_framwork.permissions import IsAuthenticated
 
-# Create your views here.
+from task.models import Task
+from api import serializers
+from permissions import IsTaskOwner
+
+
+class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = serializers.TaskSerializer
+    permission_classes = [IsAuthenticated, IsTaskOwner]
+
+
+class TaskList(genetics.ListCreateAPIView):
+    serializer_class = serializers.TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(owner=self.request.user)

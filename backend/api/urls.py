2ae7f5b69fce4ec02ba54148f1b56d92dbbbf6f4
Aleksey Fedorov <alfed13.02@mail.ru>
2021-02-28 22:54:28 +0300

    urls of django-rest-auth were added to project's urls.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 
 from api import views
 urlpatterns = [
     path('', views.TaskList.as_view()),
     path('<int:pk>/', views.TaskDetail.as_view()),
+    path('rest-auth/', include('rest_auth.urls')),
 ]

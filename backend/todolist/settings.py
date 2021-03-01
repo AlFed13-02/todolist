77e4d7b88aab55829f8fc465903c01db61df4aae
Aleksey Fedorov <alfed13.02@mail.ru>
2021-03-01 09:57:53 +0300

    Project settings were modified.

--- backend/todolist/settings.py
+++ backend/todolist/settings.py
@@ -38,12 +38,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
     'rest_framework',
     'rest_framework.authtoken',
     'rest_auth',
+    'rest_auth.registration',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    
 
     'tasks.apps.TasksConfig',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
@@ -129,6 +136,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+SITE_ID = 1
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
